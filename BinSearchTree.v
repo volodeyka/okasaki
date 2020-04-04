d1cb9e82cd45da81e41abf60d0a2313a92c46fd8
dmitromikh <mikhaylovskiy.dmitriy@gmail.com>
2020-04-04 21:53:42 +0300

    Replaced inductive definitions with functions and finished proofs

--- BinSearchTree.v
+++ BinSearchTree.v
@@ -5,132 +5,165 @@ Open Scope order_scope.
 
 
 Module BST.
-  Section BinSearchTree.
-  Notation ordType := (orderType tt).
-  Variable Elem : ordType.
+Section BinSearchTree.
+Notation ordType := (orderType tt).
+Variable Elem : ordType.
 
-  Inductive Tree := E | T : Tree -> Elem -> Tree -> Tree.
+Inductive Tree := E | T : Tree -> Elem -> Tree -> Tree.
 
-  Definition EmptyTree := E.
+Definition EmptyTree := E.
 
-  Inductive LE (x : Elem) : Tree -> Prop :=
-    | LE_E : LE x E
-    | LE_T : forall y l r (LEQ : x <= y) (LEL : LE x l) (LER : LE x r), LE x (T l y r).
+Fixpoint LT (x : Elem) (Tr : Tree) : bool :=
+if Tr is T l y r then (LT x l) && (LT x r) && (x < y) else true.
 
-  Inductive GE (x : Elem) : Tree -> Prop :=
-    | GE_E : GE x E
-    | GE_T : forall y l r (GEQ : x >= y) (GEL : GE x l) (GER : GE x r), GE x (T l y r).
+Fixpoint GT (x : Elem) (Tr : Tree) : bool :=
+if Tr is T l y r then (GT x l) && (GT x r) && (x > y) else true.
 
-  Inductive BSTOrder : Tree -> Prop :=
-    | TO_E : BSTOrder E
-    | TO_T : forall x l r (G : GE x l) (L : LE x r) 
-              (TOL : BSTOrder l) (TOR : BSTOrder r),
-              BSTOrder (T l x r).
+Fixpoint BSTOrder (Tr : Tree) : bool :=
+if Tr is T l x r then (GT x l) && (LT x r) && (BSTOrder l) && (BSTOrder r) else true.
 
-  Lemma TOC l x r :
-    BSTOrder (T l x r) ->
-    GE x l /\ LE x r /\ (BSTOrder l) /\ (BSTOrder r).
-  Proof. by move=> H; inversion H. Qed.
+Lemma TOC l x r :
+BSTOrder (T l x r) ->
+GT x l /\ LT x r /\ (BSTOrder l) /\ (BSTOrder r).
+Proof. by move=> /= /andP[/andP[/andP[]]]. Qed.
 
-  Lemma LEC x l y r :
-    LE x (T l y r) ->
-    (x <= y) /\ LE x l /\ LE x r.
-  Proof. by move=> H; inversion H. Qed.
+Lemma LTC x l y r :
+LT x (T l y r) ->
+(x < y) /\ LT x l /\ LT x r.
+Proof. by move=> /= /andP[/andP[]]. Qed.
 
-  Lemma GEC x l y r :
-    GE x (T l y r) ->
-    (x >= y) /\ GE x l /\ GE x r.
-  Proof. by move=> H; inversion H. Qed.
+Lemma GTC x l y r :
+GT x (T l y r) ->
+(x > y) /\ GT x l /\ GT x r.
+Proof. by move=> /= /andP[/andP[]]. Qed.
 
-  Lemma LEG (x x' : Elem) (Tr : Tree) : (x' <= x) /\ LE x Tr -> LE x' Tr.
-  Proof.
-    elim: Tr=> [_ | l IHl y r IHr [x'x L]]; first by apply: LE_E.
-    move: (LEC _ _ _ _ L)=> [xy] [Ll] Lr; constructor; first by apply: le_trans x'x xy.
-    - by apply: IHl.
-    by apply: IHr.
-  Qed.
+Lemma LEG (x x' : Elem) (Tr : Tree) : (x' < x) /\ LT x Tr -> LT x' Tr.
+Proof.
+elim: Tr=> [_ | l IHl y r IHr [x'x L]]; first by [].
+move: (LTC _ _ _ _ L)=> [xy] [Ll] Lr /=. apply/andP; split. apply/andP; split.
+- by apply: IHl.
+- by apply: IHr.
+by apply: (lt_trans x'x xy).
+Qed.
 
-  Lemma GEL (x x' : Elem) (Tr : Tree) : (x' >= x) /\ GE x Tr -> GE x' Tr.
-  Proof.
-    elim: Tr=> [_ | l IHl y r IHr [x'x G]]; first by apply: GE_E.
-    move: (GEC _ _ _ _ G)=> [yx] [Gl] Gr; constructor; first by apply: le_trans yx x'x.
-    - by apply: IHl.
-    by apply: IHr.
-  Qed.
+Lemma GTL (x x' : Elem) (Tr : Tree) : (x' > x) /\ GT x Tr -> GT x' Tr.
+Proof.
+elim: Tr=> [_ | l IHl y r IHr [x'x G]]; first by [].
+move: (GTC _ _ _ _ G)=> [xy] [Gl] Gr /=. apply/andP; split. apply/andP; split.
+- by apply: IHl.
+- by apply: IHr.
+by apply: (lt_trans xy x'x).
+Qed.
 
-  Fixpoint member (x : Elem) (Tr : Tree) : Prop :=
-    match Tr with
-    | E => false
-    | T a y b => if x < y then member x a else if x > y then member x b else true
-    end.
+Fixpoint member (x : Elem) (Tr : Tree) : bool :=
+match Tr with
+| E => false
+| T a y b => if x < y then member x a else if x > y then member x b else true
+end.
 
-  (** member function with at most d + 1 comparisons where d is the depth of a tree*)
-  Fixpoint candidate (x : Elem) (Tr : Tree) (cand : option Elem) : Prop :=
-    match Tr with
-    | E => if cand is Some c then if x == c then true else false
-           else false
-    | T a y b => if x < y then candidate x a cand else candidate x b (Some y)
-    end.
+(** member function with at most d + 1 comparisons where d is the depth of a tree*)
+Fixpoint candidate (x : Elem) (Tr : Tree) (cand : option Elem) : bool :=
+match Tr with
+| E => if cand is Some c then if x == c then true else false
+       else false
+| T a y b => if x < y then candidate x a cand else candidate x b (Some y)
+end.
 
-  Definition member' (x : Elem) (Tr : Tree) : Prop := candidate x Tr None.
-(** TODO: finish memberE proof *)
-  Lemma memberE (x : Elem) (Tr : Tree) (BS : BSTOrder Tr) : member x Tr = candidate x Tr None.
-  Proof.
-  Admitted.
+Definition member' (x : Elem) (Tr : Tree) : bool := candidate x Tr None.
 
-  Fixpoint insert (x : Elem) (Tr : Tree) : Tree :=
-    match Tr with
-    | E => T E x E
-    | T a y b => if x < y then T (insert x a) y b else
-                   if x > y then T a y (insert x b) else T a y b
-    end.
+Lemma nhd_member (x x' : Elem) (Tr : Tree) (BS : BSTOrder Tr) (NEQ : x != x') :
+      candidate x Tr None = candidate x Tr (Some x').
+Proof.
+elim: Tr BS=> /=. by rewrite ifN_eq.
+move=> l IHl y r IHr /andP[/andP[/andP[Gl Lr] BSl] BSr]. case: ltgtP.
+- move=> xy. by apply: IHl.
+- by [].
+by [].
+Qed.
 
-  Lemma member_LE (x x' : Elem) (Tr : Tree) : LE x Tr /\ (member x' Tr) -> x <= x'.
-  Proof.
-    elim: Tr=> [|l IHl y r IHr /= [L M]]; first by elim.
-    move: (LEC _ _ _ _ L)=> [LEQ] [Ll] Lr. case: (ltgtP x' y).
-    - by move=> x'y; rewrite x'y in M; rewrite IHl.
-    - by move=> yx'; rewrite yx' in M; rewrite lt_gtF in M; rewrite ?IHr.
-    by move=> xy; rewrite -xy in LEQ.
-  Qed.
+Lemma mem_in_hd (x : Elem) (Tr: Tree) (L : LT x Tr) : candidate x Tr (Some x).
+Proof.
+elim: Tr L=> [| l IHl x' r IHr L /=]; first by rewrite /= eqxx.
+move: (LTC _ _ _ _ L)=> [x'x [Gl Gr]]. by rewrite x'x IHl.
+Qed.
 
-  Lemma member_GE (x x' : Elem) (Tr : Tree) : GE x Tr /\ (member x' Tr) -> x >= x'.
-  Proof.
-    elim: Tr=> [|l IHl y r IHr /= [G M]]; first by elim.
-    move: (GEC _ _ _ _ G)=> [GEQ] [Gl] Gr. case: (ltgtP x' y).
-    - by move=> x'y; rewrite x'y in M; rewrite IHl.
-    - by move=> yx'; rewrite yx' in M; rewrite lt_gtF in M; rewrite ?IHr.
-    by move=> xy; rewrite -xy in GEQ.
-  Qed.
+Lemma memberE_aux (x : Elem) (Tr : Tree) (BS : BSTOrder Tr) : member x Tr = candidate x Tr None.
+Proof.
+elim: Tr BS=> [| l IHl x' r IHr BS /=]; first by [].
+move: (TOC _ _ _ BS)=> [G [L [BSl BSr]]].
+case: ltgtP.
+- move=> xx'. by apply: IHl.
+- move=> x'x. rewrite IHr; try by []. apply nhd_member; first by [].
+  by rewrite neq_lt; apply/orP; right.
+move=> eq. rewrite eq. by rewrite mem_in_hd.
+Qed.
 
-  Lemma insert_LE (x x' : Elem) (Tr : Tree) : LE x Tr /\ (x <= x') -> LE x (insert x' Tr).
-  Proof.
-    elim: Tr=> [[L LEQ] | l IHl y r IHr /= [L M]]; first by constructor.
-    move: (LEC _ _ _ _ L)=> [LEQ] [Ll] Lr.
-    case: (ltgtP x' y); try by [].
-    - move=> x'y; constructor; try by []; by apply: IHl.
-    - move=> yx'; constructor; try by []; by apply: IHr.
-  Qed.
+Theorem memberE (x : Elem) (Tr : Tree) (BS : BSTOrder Tr) : member x Tr = member' x Tr.
+Proof.
+by rewrite memberE_aux.
+Qed.
 
-  Lemma insert_GE (x x' : Elem) (Tr : Tree) : GE x Tr /\ (x >= x') -> GE x (insert x' Tr).
-  Proof.
-    elim: Tr=> [[G GEQ] | l IHl y r IHr /= [G M]]; first by constructor.
-    move: (GEC _ _ _ _ G)=> [GEQ] [Gl] Gr.
-    case: (ltgtP x' y); try by [].
-    - move=> x'y; constructor; try by []; by apply: IHl.
-    - move=> yx'; constructor; try by []; by apply: IHr.
-  Qed.
+Fixpoint insert (x : Elem) (Tr : Tree) : Tree :=
+match Tr with
+| E => T E x E
+| T a y b => if x < y then T (insert x a) y b else
+               if x > y then T a y (insert x b) else T a y b
+end.
 
-  Theorem BSTOrder_preserved (x : Elem) (Tr : Tree) (BS : BSTOrder Tr) : BSTOrder (insert x Tr).
-  Proof.
-    elim: BS=> [| x' l t Gl Lr TOL TOLx TOR TORx /=]; first by constructor; constructor.
-    case: ltgtP.
-      - move=> xx'; constructor; try by [].
-        apply: insert_GE; split. by []. by apply: (ltW xx').
-      - move=> x'x; constructor; try by [].
-        apply: insert_LE; split. by []. by apply: (ltW x'x).
-      by constructor.
-  Qed.
+Lemma member_LT (x x' : Elem) (Tr : Tree) : LT x Tr /\ (member x' Tr) -> x < x'.
+Proof.
+elim: Tr=> [|l IHl y r IHr /= [L M]]; first by elim.
+move: (LTC _ _ _ _ L)=> [LEQ] [Ll] Lr. case: (ltgtP x' y).
+- by move=> x'y; rewrite x'y in M; rewrite IHl.
+- by move=> yx'; rewrite yx' in M; rewrite lt_gtF in M; rewrite ?IHr.
+by move=> xy; rewrite -xy in LEQ.
+Qed.
 
-  End BinSearchTree.
+Lemma member_GT (x x' : Elem) (Tr : Tree) : GT x Tr /\ (member x' Tr) -> x > x'.
+Proof.
+elim: Tr=> [|l IHl y r IHr /= [G M]]; first by elim.
+move: (GTC _ _ _ _ G)=> [GEQ] [Gl] Gr. case: (ltgtP x' y).
+- by move=> x'y; rewrite x'y in M; rewrite IHl.
+- by move=> yx'; rewrite yx' in M; rewrite lt_gtF in M; rewrite ?IHr.
+by move=> xy; rewrite -xy in GEQ.
+Qed.
+
+Lemma insert_LT (x x' : Elem) (Tr : Tree) : LT x Tr /\ (x < x') -> LT x (insert x' Tr).
+Proof.
+elim: Tr=> [[L LEQ] | l IHl y r IHr /= [L M]]; first by [].
+move: (LTC _ _ _ _ L)=> [LEQ] [Ll] Lr.
+case: (ltgtP x' y); try by [].
+- move=> x'y /=; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  by apply: IHl.
+- move=> yx' /=; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  by apply: IHr.
+Qed.
+
+Lemma insert_GT (x x' : Elem) (Tr : Tree) : GT x Tr /\ (x > x') -> GT x (insert x' Tr).
+Proof.
+elim: Tr=> [[G GEQ] | l IHl y r IHr /= [G M]]; first by [].
+move: (GTC _ _ _ _ G)=> [GEQ] [Gl] Gr.
+case: (ltgtP x' y); try by [].
+- move=> x'y /=; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  by apply: IHl.
+- move=> yx' /=; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  by apply: IHr.
+Qed.
+
+Theorem BSTOrder_preserved (x : Elem) (Tr : Tree) (BS : BSTOrder Tr) : BSTOrder (insert x Tr).
+Proof.
+elim: Tr BS=> [| l IHl x' r IHr /andP[/andP[/andP[Gl Lr] BSl] BSr]] /=; first by [].
+case: ltgtP.
+- move=> xx' /=; try by [].
+  apply/andP; split; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  apply: insert_GT; split; by []. apply IHl. by apply BSl.
+- move=> x'x /=; try by [].
+  apply/andP; split; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+  apply: insert_LT; split; by []. apply IHr. by apply BSr.
+move=> eq. rewrite -eq /=.
+apply/andP; split; try by []. apply/andP; split; try by []. apply/andP; split; try by [].
+by rewrite eq. by rewrite eq.
+Qed.
+
+End BinSearchTree.
 End BST.
